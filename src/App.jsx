dc14699cb6db95309eecc8ce7b3e2809fe695419
npobbathi <44302756+npobbathi@users.noreply.github.com>
2022-10-05 11:16:38 -0700

    Imported from Quest

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import questTheme from 'src/MyDesignSystemLightTheme';
 import ProductCard1 from 'src/components/ProductCard1/ProductCard1';
+import ProductCard2 from 'src/components/ProductCard2/ProductCard2';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <ThemeProvider theme={questTheme}>
         <Routes>
           <Route path="/" element={<ProductCard1/>} />
+          <Route path="p2" element={<ProductCard2/>} />
         </Routes>
       </ThemeProvider>
     </StyledEngineProvider>
